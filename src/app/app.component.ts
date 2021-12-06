f9425e1b8851cab346f1416c0c1f93bd8e76c53e
hamzamansoorch <hamzamansoorch@gmail.com>
2021-12-06 11:18:51 +0500

    second-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Task 1 hello-world';
+export class AppComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+
+  }
+
+
+  quantity: number = 1;
+  i: number = 1;
+  plus() {
+    if (this.i != 10) {
+      this.i++;
+      this.quantity = this.i;
+    }
+  }
+
+  minus() {
+    if (this.i > 0) {
+      this.i--;
+      this.quantity = this.i;
+    }
+  }
 }
